009fbeadc88b7a3622091ee5bc549d49c7dafce4
DeylerAF <deylerafernandez@outlook.com>
2024-10-04 14:59:58 -0600

    Add project files.

---
+++ BaseLibrary/DTOs/Register.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.DTOs
+{
+    public class Register : AccountBase
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? FullName { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string? ConfirmPassword { get; set; }
+    }
+}
